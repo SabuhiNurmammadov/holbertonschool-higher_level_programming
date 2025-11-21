ba45e93f4464b4bb3dfcfeadbc2426b78c1279ec
SabuhiNurmammadov <nurmammadovsabuhi@gmail.com>
2025-11-21 08:03:23 +0000

    task 10 -v2

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -4,5 +4,5 @@ def best_score(a_dictionary):
         return None
     max_value = max(a_dictionary.values())
     for i in a_dictionary:
-        if a_dictionary(i) == max_value
+        if a_dictionary(i) == max_value:
             return i
