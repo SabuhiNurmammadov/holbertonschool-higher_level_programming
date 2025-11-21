4fc40306c092dafa24e7bcfbad4504b46a9fbb64
SabuhiNurmammadov <nurmammadovsabuhi@gmail.com>
2025-11-21 09:30:16 +0000

    task 12 -v2

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if not  isinistance(roman_string, str) or roman_string is None:
+    if not isinstance(roman_string, str) or roman_string is None:
         return 0
 
     roman_dict = {'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}
@@ -8,10 +8,11 @@ def roman_to_int(roman_string):
     prev = 0
 
     for i in reversed(roman_string):
-        value =  roman_dict[i]
+        value = roman_dict[i]
         if value < prev:
             total -= value
-        else value > prev:
+        else:
             total += value
         prev = value
+
     return total
