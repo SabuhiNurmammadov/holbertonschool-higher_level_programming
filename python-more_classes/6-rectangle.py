f1007477ae19b5b8d33454e694ddbac949e885f0
SabuhiNurmammadov <nurmammadovsabuhi@gmail.com>
2025-11-25 11:34:48 +0000

    task 8 -v2

--- python-more_classes/6-rectangle.py
+++ python-more_classes/6-rectangle.py
@@ -47,6 +47,7 @@ class Rectangle:
     def __str__(self):
         if self.__width == 0 or self.__height == 0:
             return ""
+        rect = ""
         for i in range(self.__height):
             rect += "#" * self.__width
             if i != self.__height - 1:
