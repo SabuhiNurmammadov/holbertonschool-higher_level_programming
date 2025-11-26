3498a3fcb38c559a55d7842371fe97966372de82
SabuhiNurmammadov <nurmammadovsabuhi@gmail.com>
2025-11-26 20:03:59 +0000

    task 11 -v3

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -8,7 +8,7 @@ class Square(Rectangle):
 
     def __init__(self, size):
         self.integer_validator("size", size)
-        self.__size = self
+        self.__size = size
 
     def area(self):
         return self.__size * self.__size
