f0ac1dc48e6115862e20246c2824d56b9be6fe69
SabuhiNurmammadov <nurmammadovsabuhi@gmail.com>
2025-11-28 10:51:49 +0000

    task5 -v2

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -8,4 +8,5 @@ import json
 def save_to_json_file(my_obj, filename):
     """Task 5"""
 
-    return json.dump(my_obj, filename)
+    with open(filename, "w", encoding="utf-8") as f:
+        json.dump(my_obj, f)
