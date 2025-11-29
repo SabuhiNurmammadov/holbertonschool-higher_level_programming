8d035e45fba4d37293779500ced980c6c7642487
SabuhiNurmammadov <nurmammadovsabuhi@gmail.com>
2025-11-29 18:07:12 +0000

    task 14 -v1

---
+++ SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,7 @@
+-- task 14
+SELECT tv_genres.name
+FROM tv_shows
+JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.genre_id
+WHERE tv_show.title = 'Dexter'
+ORDER BY tv_genres.name ASC
