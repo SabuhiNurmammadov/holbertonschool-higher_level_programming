4f84c411ee8bfa30ee75e5bc82d86be2f56b6ee6
SabuhiNurmammadov <nurmammadovsabuhi@gmail.com>
2025-12-02 12:55:31 +0000

    task 1 -v1

---
+++ python-network_1/1-hbtn_header.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Task 1"""
+
+
+import sys
+import urllib.request
+
+url = sys.argv[1]
+
+with urllib.request.urlopen(url) as response:
+    x_request_id = response.info().get("X-Request-Id")
+    print(x_request_id)
