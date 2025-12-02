74495f04cd92b5f73c1f7321b4770b6a8884013f
SabuhiNurmammadov <nurmammadovsabuhi@gmail.com>
2025-12-02 15:54:33 +0000

    task 8 -v1\git push

---
+++ python-network_1/8-json_api.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""TASK 8"""
+
+
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        q = sys.argv[1]
+    else:
+        q = ""
+
+    url = "http://0.0.0.0:5000/search_user"
+    values = {"q": q}
+
+    try:
+        req = requests.post(url, values)
+        if len(req.json()) == 0:
+            print("No result")
+        else:
+            print(f"[{req.json()['id']}] {req.json()['name']}")
+    except requests.exception.JSONDecodeError:
+        print("Not a valid JSON")
