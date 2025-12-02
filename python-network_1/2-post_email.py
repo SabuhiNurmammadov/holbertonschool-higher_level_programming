449e57f3125342d713174259a6385d1c56902f97
SabuhiNurmammadov <nurmammadovsabuhi@gmail.com>
2025-12-02 13:07:58 +0000

    task 2 -v1

---
+++ python-network_1/2-post_email.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Task 2"""
+
+
+import urllib.requst
+import urllib.parse
+import sys
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    mail = sys.argv[2]
+    values = {'email': mail}
+    data = urllib.parse.urlencode(values)
+    data = data.encode('utf-8')
+    req = urllib.request.Request(url, data)
+    with urllib.request.urlopen(req) as response:
+        body = response.read().decode('utf-8')
+        print(body)
