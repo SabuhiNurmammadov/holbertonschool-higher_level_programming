bb1d93c419e89540f1e1d5a4a5762b5c0c2b1ea4
SabuhiNurmammadov <nurmammadovsabuhi@gmail.com>
2025-12-02 13:16:37 +0000

    task 3 -v1

---
+++ python-network_1/3-error_code.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Task 3"""
+
+
+import urllib.request
+import urllib.error
+import sys
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    try:
+        with urllib.request.urlopen(url) as response:
+            body = response.read()
+            body = body.decode('utf-8')
+            print(body)
+    except urllib.error.HTTPerror as error:
+        print(f"Error code: {error.code}")
