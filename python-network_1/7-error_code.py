12fb94d24bfaebf1952814ba5c49af2c7fe56d30
SabuhiNurmammadov <nurmammadovsabuhi@gmail.com>
2025-12-02 15:35:04 +0000

    task 7 -v1

---
+++ python-network_1/7-error_code.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Task 7"""
+
+
+import sys
+import requests
+
+
+if __name__ == "main":
+    url = sys.argv[1]
+    try:
+        req = requests.get(url)
+        req.raise_for_status()
+        print(req.text)
+    except requests.exceptions.HTTPError as error
+        print(f"{Error code: {error.response.status_code}")
