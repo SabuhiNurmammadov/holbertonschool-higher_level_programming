5b318838ca8cf808ae8c3323a12f54d8fd4e2114
SabuhiNurmammadov <nurmammadovsabuhi@gmail.com>
2025-12-02 16:10:24 +0000

    task 9 -v4

--- python-network_1/10-my_github.py
+++ python-network_1/10-my_github.py
@@ -8,10 +8,12 @@ import sys
 
 if __name__ == "__main__":
     url = "https://api.github.com/user"
-    headers = {"Authorization": f"token {sys.argv[2]}"}
+    username = sys.argv[1]
+    token = sys.argv[2]
 
     try:
-        req = requests.get(url, headers = headers)
-        print(reqx.json()['id'])
+        req = requests.get(url, auth=(username, token))
+        req.raise_for_status()
+        print(req.json()['id'])
     except Exception:
         print(None)
