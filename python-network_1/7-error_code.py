c7f98cbfea548de65d9f49ae021f589e18447c69
SabuhiNurmammadov <nurmammadovsabuhi@gmail.com>
2025-12-02 15:36:42 +0000

    task 7 -v2

--- python-network_1/7-error_code.py
+++ python-network_1/7-error_code.py
@@ -6,11 +6,11 @@ import sys
 import requests
 
 
-if __name__ == "main":
+if __name__ == "__main__":
     url = sys.argv[1]
     try:
         req = requests.get(url)
         req.raise_for_status()
         print(req.text)
-    except requests.exceptions.HTTPError as error
+    except requests.exceptions.HTTPError as error:
         print(f"{Error code: {error.response.status_code}")
