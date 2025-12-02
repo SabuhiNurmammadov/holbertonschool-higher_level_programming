0e3835a5a36199b5e8e5f13c9d65fc7195bc5397
SabuhiNurmammadov <nurmammadovsabuhi@gmail.com>
2025-12-02 12:42:25 +0000

    task 0 -v4

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -6,10 +6,9 @@ import urllib.request
 
 
 url = 'https://intranet.hbtn.io/status'
-
 with urllib.request.urlopen(url) as response:
     body = response.read() # bu bytes qaytarir
     print("Body response:")
-    print(f"\t- type: {type(body)}") 
+    print(f"\t- type: {type(body)}")
     print(f"\t- content: {body}")
-    print(f"\t- utf8 content: {body.decode('utf-8')}") #utf-8 
+    print(f"\t- utf8 content: {body.decode('utf-8')}") #utf-8
