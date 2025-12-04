785b4e50fd35a9780e1a7d2380900f9c0043a059
SabuhiNurmammadov <nurmammadovsabuhi@gmail.com>
2025-12-04 10:25:36 +0000

    task 3 -v1

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -1,17 +1,12 @@
 #!/usr/bin/python3
 """TASK 3"""
 
-
 import json
 import http.server
 
-
 PORT = 8000
 
-
 class SimpleHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
-    """Task 3"""
-
 
     def do_GET(self):
         if self.path == "/data":
@@ -20,27 +15,32 @@ class SimpleHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
             self.send_header("Content-Type", "application/json")
             self.end_headers()
             self.wfile.write(json.dumps(data).encode("utf-8"))
+
         elif self.path == "/status":
             self.send_response(200)
-            self.send_header("Content-Type", "application/json")
+            self.send_header("Content-Type", "text/plain")
             self.end_headers()
             self.wfile.write(b"OK")
+
         elif self.path == "/":
             self.send_response(200)
             self.send_header("Content-Type", "text/plain")
             self.end_headers()
             self.wfile.write(b"Hello, this is a simple API!")
+
         else:
             self.send_response(404)
             self.send_header("Content-Type", "text/plain")
             self.end_headers()
             self.wfile.write(b"Endpoint not found")
 
+
 def run(server_class=http.server.HTTPServer, handler_class=SimpleHTTPRequestHandler):
     server_address = ("", PORT)
-    httpd = HTTPServer(server_address, SimpleAPIHandler)
+    httpd = server_class(server_address, handler_class)
     print(f"Server running on port {PORT}...")
     httpd.serve_forever()
 
+
 if __name__ == "__main__":
-    run())
+    run()
